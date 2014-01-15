34ae52d5bea7092a422e0ecfce95bca5573dc2da
Michael Comella <michael.l.comella@gmail.com>
2014-01-15 12:44:32 -0800

    Addon parses CNN on loadIntoWindow.

--- bootstrap.js
+++ bootstrap.js
@@ -1,12 +1,51 @@
 const Cc = Components.classes;
 const Ci = Components.interfaces;
+const Cu = Components.utils;
+
+Cu.import('resource://gre/modules/Services.jsm');
+
+const EXAMPLE_URI = 'http://rss.cnn.com/rss/cnn_topstories.rss';
 
 function loadIntoWindow(window) {
   if (!window) { return; }
+  parseFeed(EXAMPLE_URI);
 }
 
 function unloadFromWindow(window) {
   if (!window) { return; }
+}
+
+function parseFeed(rssURL) {
+  // via mfinkle.
+  let listener = {
+    handleResult: function handleResult(feedResult) {
+      Services.console.logStringMessage('VERSION: ' + feedResult.version);
+      let feedDoc = feedResult.doc;
+      let feed = feedDoc.QueryInterface(Ci.nsIFeed);
+      if (feed.items.length == 0)
+        return;
+      Services.console.logStringMessage('FEED TITLE: ' + feed.title.plainText());
+      for (let i = 0; i < feed.items.length; ++i) {
+        let entry = feed.items.queryElementAt(i, Ci.nsIFeedEntry);
+        entry.QueryInterface(Ci.nsIFeedContainer);
+        Services.console.logStringMessage('SUMMARY: ' + entry.summary.plainText());
+      }
+    }
+  };
+
+  let xhr = Cc['@mozilla.org/xmlextras/xmlhttprequest;1'].createInstance(Ci.nsIXMLHttpRequest);
+  xhr.open('GET', rssURL, true);
+  xhr.overrideMimeType('text/xml');
+
+  xhr.addEventListener('load', (function() {
+    if (xhr.status == 200) {
+      let processor = Cc['@mozilla.org/feed-processor;1'].createInstance(Ci.nsIFeedProcessor);
+      processor.listener = listener;
+      let uri = Services.io.newURI(rssURL, null, null);
+      processor.parseFromString(xhr.responseText, uri);
+    }
+  }), false);
+  xhr.send(null);
 }
 
 function install(aData, aReason) {}
